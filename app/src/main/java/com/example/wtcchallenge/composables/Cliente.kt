48097c70c58210822c7962d5f8ca0b17b6866417
Aline de Oliveira Ciriaco <aline.id@hotmail.com.br>
2025-10-30 14:50:45 -0300

    Ajustes

--- app/src/main/java/com/example/wtcchallenge/composables/Cliente.kt
+++ app/src/main/java/com/example/wtcchallenge/composables/Cliente.kt
@@ -4,7 +4,7 @@ package com.example.wtcchallenge.composables
 // 1️⃣ DATA CLASS – MODELO DE CLIENTE
 // =========================================================
 // Representa a estrutura dos dados de cada cliente
-data class Cliente(
+data class Client(
     val nome: String,
     val numero: String,
     val ramo: String,
